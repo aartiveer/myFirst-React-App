58110c2d3d74f76e85ae7c217eff980084b0dffb
aartiveer <aartiveer1212@gmail.com>
2025-05-15 02:00:12 +0530

    Add second section AboutSection.js with it's images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import HeroSection from  "../components/HeroSection"; 
 import SocialIcons from "../components/SocialIcons";
+import AboutSection from '../components/AboutSection';
+import ExpertiseSection from '../components/ExpertiseSection';
+
 
 const Home = () => (
     <div>
         <HeroSection />
         <SocialIcons />
+        <AboutSection />
+        <ExpertiseSection />
     </div>
 );
 
