f2a06f57f75c752dd2ddca625b9bf727b23bd608
aartiveer <aartiveer1212@gmail.com>
2025-05-15 02:33:19 +0530

    Add VisionSection.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import HeroSection from  "../components/HeroSection";
 import SocialIcons from "../components/SocialIcons";
 import AboutSection from '../components/AboutSection';
 import ExpertiseSection from '../components/ExpertiseSection';
+import VisionSection from '../components/VisionSection';
 
 
 const Home = () => (
@@ -12,6 +13,7 @@ const Home = () => (
         <SocialIcons />
         <AboutSection />
         <ExpertiseSection />
+        <VisionSection />
     </div>
 );
 
